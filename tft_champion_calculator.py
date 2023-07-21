0ee2e91cdc290898c94cf82c490c5b473cd04979
Stephen Cowan <stephencowan0@gmail.com>
2023-07-21 12:55:22 -0600

    finished dictionaries

--- tft_champion_calculator.py
+++ tft_champion_calculator.py
@@ -1,3 +1,4 @@
+# Creates the dictionary of all commons, setting availibility to 29
 commons = {}
 common_file = open("commons.csv", "r")
 common_list = common_file.readlines()
@@ -5,3 +6,39 @@ for champion in common_list:
     champion = champion.rstrip()
     commons[champion] = 29
 print(commons)
+
+# Creates the dictionary of all uncommons, setting availibility to 22
+uncommons = {}
+uncommon_file = open("uncommons.csv", "r")
+uncommon_list = uncommon_file.readlines()
+for champion in uncommon_list:
+    champion = champion.rstrip()
+    uncommons[champion] = 22
+print(uncommons)
+
+# Creates the dictionary of all rares, setting availibility to 18
+rares = {}
+rare_file = open("rares.csv", "r")
+rare_list = rare_file.readlines()
+for champion in rare_list:
+    champion = champion.rstrip()
+    rares[champion] = 18
+print(rares)
+
+# Creates the dictionary of all epics, setting availibility to 12
+epics = {}
+epic_file = open("epics.csv", "r")
+epic_list = epic_file.readlines()
+for champion in epic_list:
+    champion = champion.rstrip()
+    epics[champion] = 12
+print(epics)
+
+# Creates the dictionary of all legendaries, setting availibility to 10
+legendaries = {}
+legendarie_file = open("legendaries.csv", "r")
+legendarie_list = legendarie_file.readlines()
+for champion in legendarie_list:
+    champion = champion.rstrip()
+    legendaries[champion] = 10
+print(legendaries)
