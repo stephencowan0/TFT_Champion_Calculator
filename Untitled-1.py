38be6291a47045d6985dcbc6f6f1c67a3fc054fe
Stephen Cowan <70290207+stephencowan0@users.noreply.github.com>
2023-07-21 12:50:11 -0600

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,7 +0,0 @@
-commons = {}
-common_file = open("commons.csv", "r")
-common_list = common_file.readlines()
-for champion in common_list:
-    champion = champion.rstrip()
-    commons[champion] = 29
-print(commons)
