e01e01e5bd16d4439c177c627c705d6e4692f8cd
Stephen Cowan <stephencowan0@gmail.com>
2023-07-21 13:43:33 -0600

    reads from file how many of each champion is taken and calculates the remaining based on that

--- tft_champion_calculator.py
+++ tft_champion_calculator.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
+
+# Creates the levels dataframe
 dfLevels = pd.read_csv("levels.csv")
-# more options can be specified also
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
     print(dfLevels)
 
@@ -12,7 +13,8 @@ common_list = common_file.readlines()
 for champion in common_list:
     champion = champion.rstrip()
     commons[champion] = 29
-print(commons)
+common_file.close()
+common_count = len(commons)
 
 # Creates the dictionary of all uncommons, setting availibility to 22
 uncommons = {}
@@ -21,7 +23,8 @@ uncommon_list = uncommon_file.readlines()
 for champion in uncommon_list:
     champion = champion.rstrip()
     uncommons[champion] = 22
-print(uncommons)
+uncommon_file.close()
+uncommon_count = len(uncommons)
 
 # Creates the dictionary of all rares, setting availibility to 18
 rares = {}
@@ -30,7 +33,8 @@ rare_list = rare_file.readlines()
 for champion in rare_list:
     champion = champion.rstrip()
     rares[champion] = 18
-print(rares)
+rare_file.close()
+rare_count = len(rares)
 
 # Creates the dictionary of all epics, setting availibility to 12
 epics = {}
@@ -39,7 +43,8 @@ epic_list = epic_file.readlines()
 for champion in epic_list:
     champion = champion.rstrip()
     epics[champion] = 12
-print(epics)
+epic_file.close()
+epic_count = len(epics)
 
 # Creates the dictionary of all legendaries, setting availibility to 10
 legendaries = {}
@@ -48,4 +53,56 @@ legendarie_list = legendarie_file.readlines()
 for champion in legendarie_list:
     champion = champion.rstrip()
     legendaries[champion] = 10
-print(legendaries)
+legendarie_file.close()
+legendarie_count = len(legendaries)
+
+# Read the number of each champion taken from the champions_taken file
+# And use this to calculate how much of each champion is left currently
+champions_taken_file = open("champions_taken.csv", "r")
+champions_taken_list = champions_taken_file.readlines()
+total_champion_spot = 0
+
+# Calculates how many of each common are left
+current_rarity = 0
+while current_rarity < common_count:
+    current_line_of_data = champions_taken_list[total_champion_spot].rstrip().split(
+        ',')
+    commons[current_line_of_data[0]] -= int(current_line_of_data[1])
+    current_rarity += 1
+    total_champion_spot += 1
+
+# Calculates how many of each uncommon are left
+current_rarity = 0
+while current_rarity < uncommon_count:
+    current_line_of_data = champions_taken_list[total_champion_spot].rstrip().split(
+        ',')
+    uncommons[current_line_of_data[0]] -= int(current_line_of_data[1])
+    current_rarity += 1
+    total_champion_spot += 1
+
+# Calculates how many of each rare are left
+current_rarity = 0
+while current_rarity < rare_count:
+    current_line_of_data = champions_taken_list[total_champion_spot].rstrip().split(
+        ',')
+    rares[current_line_of_data[0]] -= int(current_line_of_data[1])
+    current_rarity += 1
+    total_champion_spot += 1
+
+# Calculates how many of each epic are left
+current_rarity = 0
+while current_rarity < epic_count:
+    current_line_of_data = champions_taken_list[total_champion_spot].rstrip().split(
+        ',')
+    epics[current_line_of_data[0]] -= int(current_line_of_data[1])
+    current_rarity += 1
+    total_champion_spot += 1
+
+# Calculates how many of each legendarie are left
+current_rarity = 0
+while current_rarity < legendarie_count:
+    current_line_of_data = champions_taken_list[total_champion_spot].rstrip().split(
+        ',')
+    legendaries[current_line_of_data[0]] -= int(current_line_of_data[1])
+    current_rarity += 1
+    total_champion_spot += 1
